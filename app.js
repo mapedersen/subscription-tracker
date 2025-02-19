4bdf4eded22047bd0a71c38ba63fbcb55a28aad6
marcus pedersen <marcus@mpedersen.me>
2025-02-19 10:50:18 +0100

    Update Routes

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ import express from "express";
 
 import { PORT } from "./config/env.js";
 
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subsription.route.js";
+
 const app = express();
+
+// Api Routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");

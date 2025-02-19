10d902ffe8c8a0786764adcd1642815b57ebc697
marcus pedersen <marcus@mpedersen.me>
2025-02-19 12:25:07 +0100

    Add DB

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from "express";
 
 import { PORT } from "./config/env.js";
 
+import connectToDatabase from "./database/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subsription.route.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 
@@ -17,6 +19,9 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Subscription Tracker is running on http://localhost:${PORT}`);
+
+  connectToDatabase();
 });
+
